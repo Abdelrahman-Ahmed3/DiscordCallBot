a281ed664e7ac1016c4ac29e2dec6c832ae307e4
Abdelrahman <tny16426@gmail.com>
2025-09-20 00:07:00 +0300

    added !cfg to dump the config file as a txt

--- main.py
+++ main.py
@@ -86,6 +86,10 @@ async def setup(ctx): #!setup sends an embed with two reactions to monitor in th
     config["optin_message_id"] = msg.id
     save_config(config)
 
+async def cfg(ctx):
+    with open("config_dump.txt", "w") as f:
+        json.dump(config, f, indent=4)
+    await ctx.send(file=discord.File("config_dump.txt"))
 
 @bot.event
 async def on_voice_state_update(member, before, after): #checks if any member joins the waiting channel with the target role and then moves them to target channel
